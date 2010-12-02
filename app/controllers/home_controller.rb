a6a6ecdf4d672de161a8bda9fdce258860fa6c93
mkchandler <mkchandler@gmail.com>
2010-12-01 23:28:28 -0600

    Modified the order of the rankings.  It now has a secondary sort for points per game.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 	def index
 		@games = Game.all
     @players = Player.all
-    @players.sort! { |a,b| b.winning_pct <=> a.winning_pct }
+    @players.sort! { |a,b| (b.winning_pct <=> a.winning_pct).nonzero? || (b.points_per_game <=> a.points_per_game) }
 	end
 end
